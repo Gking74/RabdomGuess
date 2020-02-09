261c7a9ec94e5c52571dd45ad722fbfabffa87ba
Gking74 <60074869+Gking74@users.noreply.github.com>
2020-02-09 15:40:01 -0600

    Initial commit

---
+++ app/src/main/java/edu/student/rabdomguess/MainActivity.kt
@@ -0,0 +1,47 @@
+package edu.student.rabdomguess
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var number = ((Math.random() * 100001) +1).toInt()
+
+        Toast.makeText( this, "Number to remember: ${number}", Toast.LENGTH_LONG).show()
+        val txtGuess = findViewById<EditText>(R.id.txtGuess)
+        val btnGuess= findViewById<Button>(R.id.btnGuess)
+        val btnToast= findViewById<Button>(R.id.btnToast)
+        btnGuess.setOnClickListener{
+            if(number == txtGuess.text.toString().toInt()) {
+                Toast.makeText(this, "Correct, Great Job remembering!!", Toast.LENGTH_LONG).show()
+                txtGuess.setText("")
+            }
+            else{
+                Toast.makeText(this, "Incorrect, it's the not the number shown", Toast.LENGTH_LONG).show()
+                txtGuess.setText("")
+            }
+            hideKeyBoard()
+        }
+        btnToast.setOnClickListener{
+            number = ((Math.random() * 100001) +1).toInt()
+            println(number)
+            Toast.makeText(this, "Number to remember: ${number}", Toast.LENGTH_LONG).show()
+        }
+    }
+    fun hideKeyBoard()
+    {
+        try{
+            val imm = getSystemService(Context.INPUT_METHOD_SERVICE)as InputMethodManager
+            imm.hideSoftInputFromWindow(currentFocus!!.windowToken, 0)
+        } catch (e:Exception) {}
+    }
+}
